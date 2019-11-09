ed405822225007c1765a04502e39b19ce74852a5
Sebastian <isebasus@gmail.com>
2019-11-09 14:35:01 -0800

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const hostname = '127.0.0.1';
+const port = 8080;
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/pctf.html'));
+  });
+  
+  app.use('/', router);
+  
+  
+  app.listen(port, hostname, () => {
+    console.log(`App running at http://${hostname}:${port}/`);
+  });
