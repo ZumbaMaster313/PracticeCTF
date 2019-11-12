b1ea14add903fe4b1ea729655cc3766f2410761c
Sebastian <isebasus@gmail.com>
2019-11-11 17:12:11 -0800

    lol

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+var bodyParser = require("body-parser");
 const app = express();
 const hostname = '127.0.0.1';
 const port = 8080;
 const router = express.Router();
 
+app.set("view engine", "ejs");
+
+app.set("views", __dirname+"/views");
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/pctf.html'));
+  const style = "none"
+  res.render("index", {challenge: "", style: style});
   });
 
-router.get('/challenge', function(req, res) {
-    res.sendFile(path.join(__dirname+'/challenge.html'));
-  });
+router.get('/superhardchallenge', function(req, res){
+  const chal = "decrypt 5A4739756458513D";
+  const style = "block"
+  res.render("index", {challenge: chal, style: style});
+
+});
+
+router.get('/challengebad', function(req, res){
+  
+});
+
+router.get('/gudChallenge', function(req, res){
+  
+});
+
+router.get('/badChallenge', function(req, res){
+  
+});
+
   
   app.use('/', router);
   
